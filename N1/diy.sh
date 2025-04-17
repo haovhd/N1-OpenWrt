aa37c33644409b1ea26c38dcdaef48f440db82ec
hao133 <36333687+hao133@users.noreply.github.com>
2025-04-18 01:03:01 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -2,11 +2,28 @@
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+#rm -rf feeds/luci/applications/luci-app-passwall
+#cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+# add luci-app-mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.2.8/g' package/base-files/files/bin/config_generate
+
+#修改默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
+
+# 修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
 
 # Clean packages
 rm -rf clone

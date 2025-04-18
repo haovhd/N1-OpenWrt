846f982dec52d9c5b5ffc339834c9ec96e06de92
haovhd <haovhd@gmail.com>
2025-04-18 16:57:02 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -6,7 +6,7 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 
 # Update packages
 #rm -rf feeds/luci/applications/luci-app-passwall
-#cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang

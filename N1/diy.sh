d1c6ae624648d66b1fb932088cb293fbc0a73e4a
hao133 <36333687+hao133@users.noreply.github.com>
2025-04-22 08:37:32 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Add packages
-#git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 #git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
 # Update packages
 #rm -rf feeds/luci/applications/luci-app-passwall
-#cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # add luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
